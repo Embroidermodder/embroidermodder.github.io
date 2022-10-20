324495f8b1c935917e3cf6d8570c55a015a53749
robin-swift <robinswiftart@gmail.com>
2022-10-20 21:38:18 +0100

    updating docs

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-DOCS_=content/docs
-
 git submodule init
 git submodule update
 
@@ -13,38 +11,55 @@ cd libembroidery
 git pull origin main
 cd ..
 
-rm $DOCS_/libembroidery.md
-rm $DOCS_/embroiderbot.md
+cd content/docs
 
-touch $DOCS_/libembroidery.md
-touch $DOCS_/embroiderbot.md
+mkdir embroidermodder_manual
+mkdir libembroidery_manual
+mkdir embroiderbot_manual
 
-cat <<EOF >>$DOCS_/libembroidery.md
+cat <<EOF >embroidermodder_manual/_index.md
 +++
-title = "The Libembroidery 1.0-alpha Manual"
+title = "Embroidermodder 2.0.0-alpha"
 author = "The Embroidermodder Team"
-abbrev = "libembroidery"
+abbrev = "embroiderbot"
 date = "2022-09-19"
 weight = 11
 +++
 EOF
 
-cat <<EOF >>$DOCS_/embroiderbot.md
+cat <<EOF >libembroidery_manual/_index.md
 +++
-title = "The EmbroiderBot 1.0-alpha Manual"
+title = "Libembroidery 1.0-alpha"
 author = "The Embroidermodder Team"
-abbrev = "embroiderbot"
+abbrev = "libembroidery"
 date = "2022-09-19"
 weight = 12
 +++
 EOF
 
-cat libembroidery/README.md >> $DOCS_/libembroidery.md
-cat libembroidery/EmbroiderBot/README.md >> $DOCS_/embroiderbot.md
-cat embroidermodder/README.md >> $DOCS_/embroidermodder.md
+cat <<EOF >embroiderbot_manual/_index.md
++++
+title = "EmbroiderBot 1.0-alpha"
+author = "The Embroidermodder Team"
+abbrev = "embroiderbot"
+date = "2022-09-19"
+weight = 13
++++
+EOF
 
-pandoc -o $DOCS_/libembroidery_0.1_manual.pdf $DOCS_/libembroidery.md
-pandoc -o $DOCS_/embroidermodder_2.0.0-alpha_manual.pdf $DOCS_/manual.md
-pandoc -o $DOCS_/embroiderbot_0.1_manual.pdf $DOCS_/embroiderbot.md
+cat ../../libembroidery/README.md >> libembroidery_manual/_index.md
+cat ../../libembroidery/EmbroiderBot/README.md >> embroiderbot_manual/_index.md
+cat ../../embroidermodder/README.md >> embroidermodder_manual/_index.md
+
+cp -R ../../libembroidery/images libembroidery_manual
+cp -R ../../libembroidery/EmbroiderBot/images embroiderbot_manual
+cp -R ../../embroidermodder/images embroidermodder_manual
+
+pandoc -o libembroidery_0.1_manual.pdf \
+    libembroidery_manual/_index.md
+pandoc -o embroidermodder_2.0.0-alpha_manual.pdf \
+    embroidermodder_manual/_index.md
+pandoc -o embroiderbot_0.1_manual.pdf \
+    embroiderbot_manual/_index.md
 
 hugo --minify

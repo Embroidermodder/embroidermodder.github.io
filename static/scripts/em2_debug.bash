743678812d1feb606e4d8a01dc7639748b702438
Robin Swift <87496244+robin-swift@users.noreply.github.com>
2022-11-10 13:40:18 +0000

    Update em2_debug.bash

--- static/scripts/em2_debug.bash
+++ static/scripts/em2_debug.bash
@@ -9,19 +9,20 @@ case "$(uname -s)" in
 Linux*)
     sudo apt-get update
     sudo apt-get install libx11-6
-    gcc -O2 -g -Wall -std=c99 -Iextern/libembroidery/src extern/libembroidery/src/*.c em2.c -o embroidermodder -lX11 -lm
+    gcc -O2 -g -Wall -std=c99 -Iextern/libembroidery/src extern/libembroidery/src/*.c src/*.c -o embroidermodder -lX11 -lm
     ;;
 Darwin*)
-    gcc -O2 -g -Wall -std=c99 -Iextern/libembroidery/src extern/libembroidery/src/*.c em2.c -o embroidermodder -lX11 -lm
+    gcc -O2 -g -Wall -std=c99 -Iextern/libembroidery/src extern/libembroidery/src/*.c src/*.c -o embroidermodder -lX11 -lm
     ;;
 CYGWIN*)
-    gcc -municode -O2 -g -Wall -std=c99 -Iextern/libembroidery/src extern/libembroidery/src/*.c em2.c -o embroidermodder -lGdi32
+    gcc -municode -O2 -g -Wall -std=c99 -Iextern/libembroidery/src extern/libembroidery/src/*.c src/*.c -o embroidermodder -lGdi32
     ;;
 MINGW*)
-    gcc -municode -O2 -g -Wall -std=c99 -Iextern/libembroidery/src extern/libembroidery/src/*.c em2.c -o embroidermodder -lGdi32
+    gcc -municode -O2 -g -Wall -std=c99 -Iextern/libembroidery/src extern/libembroidery/src/*.c src/*.c -o embroidermodder -lGdi32
     ;;
 *)
-    echo "Unrecognised system: failed to build."
+    echo "Unrecognised system: building as X11."
+    gcc -O2 -g -Wall -std=c99 -Iextern/libembroidery/src extern/libembroidery/src/*.c src/*.c -o embroidermodder -lX11 -lm
 esac
 
 timeout 10 ./embroidermodder --test &> test_results.txt
